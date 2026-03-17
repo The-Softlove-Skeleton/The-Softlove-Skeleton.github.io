cc7364d2e18ed5bf510c9d0f6b8d31b3857bc9d3
The-Softlove-Skeleton <toa-matoro@web.de>
2026-03-17 20:43:47 +0100

    Basic input handling

---
+++ script.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+    var cursorInput = document.getElementById("cursor-input")
+    cursorInput.focus()
+    cursorInput.addEventListener("keydown", function(e){
+        if(e.code === "Enter"){
+            inputSubmitted(cursorInput.value)
+            cursorInput.value = ""
+        }
+    })
+    cursorInput.onblur = function(){
+        cursorInput.focus()
+    }
+
+}
+
+function inputSubmitted(input){
+    console.log(input)
+    var outputArea = document.getElementById("output-area")
+    outputArea.innerHTML = ""
+     if(input === "ls"){
+        outputArea.innerHTML = "Directory output"
+     }else{
+        outputArea.innerHTML = "Invalid input command: "+ "'"+input+"'"
+     }
+}
